c69cfc9431530768cbb064821cb941499eba1be7
namaninaresh <namaninaresh505@gmail.com>
2021-03-30 19:59:31 +0530

    changed port

--- models/DataController.js
+++ models/DataController.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "chinna",
+  host: "database-1.cqtgb3zwauvb.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "chinnanamani",
   database: "bloodbank",
   multipleStatements: true,
 });
--- models/db.js
+++ models/db.js
@@ -1,15 +1,16 @@
-'user strict';
+"user strict";
 //local mysql db connection
-var mysql = require('mysql');
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'chinna',
-  database: 'bloodbank'
+  host: "database-1.cqtgb3zwauvb.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  password: "chinnanamani",
+  database: "bloodbank",
+  multipleStatements: true,
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
+connection.connect(function (err) {
+  if (err) throw err;
 });
 
 module.exports = connection;
